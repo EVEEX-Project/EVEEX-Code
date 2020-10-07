b7599d2066e9b528e308d23df2cc26f71bccc26b
JeanNoelClink <70959111+JeanNoelClink@users.noreply.github.com>
2020-10-07 10:45:00 +0200

    Update espacements

--- python_prototype/huffman.py
+++ python_prototype/huffman.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-
 from logger import Logger
+
+##############################################################################
 
 
 class Noeud:
-
+    
     def __init__(self, valeur, frequence, gauche=None, droite=None):
         self.frequence = frequence
         self.valeur = valeur
         self.droite = droite
         self.gauche = gauche
-
+    
+    
     def __repr__(self):
         return f"Fréquence : {self.frequence}, Valeur = {self.valeur}"
-
+    
+    
     def display(self):
         lines, *_ = self.display_aux()
         for l in lines:
             print(l)
-
+    
+    
     def display_aux(self):
         if self.droite is None and self.gauche is None:
             line = f"{self.valeur}"
@@ -28,7 +32,7 @@ class Noeud:
             height = 1
             middle = width // 2
             return [line], width, height, middle
-
+        
         if self.droite is None:
             lines, n, p, x = self.gauche.display_aux()
             s = f"{self.valeur}"
@@ -37,7 +41,7 @@ class Noeud:
             second_line = x * " " + (n - x - 1 + u) * " "
             shifted_lines = [line + u * " " for line in lines]
             return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u//2
-
+        
         if self.gauche is None:
             lines, n, p, x = self.droite.display_aux()
             s = f"{self.valeur}"
@@ -46,7 +50,7 @@ class Noeud:
             second_line = (u + x) * " " + "\\" + (n - x - 1) * " "
             shifted_lines = [u * " " + line for line in lines]
             return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
-
+        
         left, n, p, x = self.gauche.display_aux()
         right, m, q, y = self.droite.display_aux()
         s =  f"[0 1]"
@@ -66,28 +70,29 @@ class Noeud:
 
 
 class Huffman:
-
+    
     def __init__(self, phrase=None):
         self.noeuds = []
         self.symbols = {}
         self.dict = {}
         self.phrase = phrase
-
+        
         if phrase is not None:
             self.split_phrase_in_nodes(self.phrase)
-
+            
             while len(self.noeuds) > 1:
                 m1, m2, reste = self.get_two_lowest_symbols()
                 m = self.merge_two_nodes(m1, m2)
                 self.noeuds = self.sort_nodes(reste + [m])
-
+            
             self.dict = self.generate_dict(self.noeuds[0])
             
             # sans ces 2 lignes, si la phrase est de taille 1, l'unique valeur
             # de ce dico sera un str vide, ce que l'on ne veut pas !
             if len(self.phrase) == 1:
                 self.dict[self.phrase[0]] = '0'
-
+    
+    
     def split_phrase_in_nodes(self, phrase):
         self.symbols = {}
         self.noeuds = []
@@ -96,57 +101,62 @@ class Huffman:
                 self.symbols[c] += 1
             else:
                 self.symbols[c] = 1
-
+        
         for key in self.symbols:
             self.noeuds.append(Noeud(key, self.symbols[key]))
-
+        
         return self.noeuds
-
+    
+    
     def merge_two_nodes(self, noeud_a, noeud_b):
         return Noeud([noeud_a.valeur, noeud_b.valeur], noeud_a.frequence + noeud_b.frequence, noeud_a, noeud_b)
-
+    
+    
     def get_two_lowest_symbols(self):
         def get_score(noeud):
             return noeud.frequence
-
+        
         tmp = self.noeuds[:]
-
+        
         min1 = min(tmp, key=get_score)
         tmp.remove(min1)
-
+        
         min2 = min(tmp, key=get_score)
         tmp.remove(min2)
-
+        
         return min1, min2, tmp
-
+    
+    
     def sort_nodes(self, liste_noeuds = None):
         if liste_noeuds != None:
             return sorted(liste_noeuds, key=lambda x:x.frequence, reverse=True)
         else:
             self.noeuds = sorted(self.noeuds, key=lambda x:x.frequence, reverse=True)
-
+    
+    
     def generate_dict(self, racine, prefixe=""):
         if racine.gauche is None and racine.droite is None:
             return {racine.valeur: prefixe}
-
+        
         res = {}
         if racine.gauche is not None:
             res.update(self.generate_dict(racine.gauche, prefixe + "0"))
         if racine.droite is not None:
             res.update(self.generate_dict(racine.droite, prefixe + "1"))
         return res
-
+    
+    
     @staticmethod
     def reverse_dict(entry_dict):
         res = {}
         for key in entry_dict:
             res[entry_dict[key]] = key
         return res
-
-
+    
+    
     def encode_phrase(self, phrase = None, dictionnary = None):
         res = ""
-
+        
         if phrase is None:
             phrase = self.phrase
 
@@ -156,16 +166,16 @@ class Huffman:
             else:
                 res += self.dict[l]
         return res
-
-
+    
+    
     @staticmethod
     def encode_ascii(phrase):
         res = ""
         for l in phrase:
             res += str(bin(ord(l))[2:])
         return res
-
-
+    
+    
     def decode_phrase(self, enc, dico = None):
         buffer = ""
         res = ""
@@ -196,7 +206,8 @@ class Huffman:
                 res.append(dico[buffer])
                 buffer = ""
         return res
-
+    
+    
     def dictToBin(self):
         bina=''
         for cle,valeur in self.dict.items():
@@ -212,7 +223,8 @@ class Huffman:
             bina+=c
         bina+='0'
         return bina
-
+    
+    
     @staticmethod
     def binToDict(bina):
         k=0
@@ -246,10 +258,15 @@ if __name__ == "__main__":
     phrase = [(2, 3), (4, 6), (13, 9), (2, 0)]
     huff = Huffman(phrase)
     huff.noeuds[0].display()
+    
     Logger.get_instance().start_file_logging("log.log")
+    
     Logger.get_instance().debug("Symbols : " + str(huff.symbols))
     Logger.get_instance().debug("Dictionnary : " + str(huff.dict))
-    Logger.get_instance().debug("Encoded : " + str(huff.encode_phrase()))
-    Logger.get_instance().debug('Decoded : ' + str(Huffman.decode_frame_RLE(huff.encode_phrase(), huff.dict)))
-    Logger.get_instance().debug('Encoded Dictionnary : ' + str(huff.dictToBin()))
-    Logger.get_instance().debug('Decoded Dictionnary : ' + str(Huffman.binToDict(huff.dictToBin())))
+    
+    Logger.get_instance().debug("Encoded Frame : " + str(huff.encode_phrase()))
+    Logger.get_instance().debug("Decoded Frame : " + str(Huffman.decode_frame_RLE(huff.encode_phrase(), huff.dict)))
+    
+    Logger.get_instance().debug("Encoded Dictionnary : " + str(huff.dictToBin()))
+    Logger.get_instance().debug("Decoded Dictionnary : " + str(Huffman.binToDict(huff.dictToBin())))
+
