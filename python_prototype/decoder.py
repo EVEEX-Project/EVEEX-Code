6556820cd24621b9fd7774bba5218e5e259f9788
JeanNoelClink <70959111+JeanNoelClink@users.noreply.github.com>
2020-10-20 10:13:38 +0200

    Fixs mineurs décodeur (pour le main)

--- python_prototype/decoder.py
+++ python_prototype/decoder.py
@@ -11,7 +11,7 @@ class Decoder:
 
     def __init__(self):
         pass
-
+    
     def decode_bitstream_RLE(self, bitstream):
         """
         Permet de décoder un bitstream associé une frame RLE avec seulement 
@@ -25,7 +25,7 @@ class Decoder:
         """
         rle_data = BitstreamGenerator.decode_bitstream_RLE(bitstream)
         return rle_data
-
+    
     def decode_run_length(self, rle_data):
         """
         Décode les paires de valeurs issues de la RLE et retourne
@@ -37,16 +37,14 @@ class Decoder:
         Returns:
             quantized_data: liste de valeurs quantifiées
         """
-
+        
         tab = np.int_([])
         for x in rle_data:
             tab = np.concatenate((tab, np.int_(np.zeros(x[0]))))
             tab = np.concatenate((tab, np.array([x[1]])))
-
-        # On divise en 3 la suite de valeurs pour avoir les 3 couches :
-        tab = np.array_split(tab, 3)
+        
         return tab
-
+    
     def decode_zigzag(self, quantized_data):
         """
         Decode une liste de valeurs quantifiées en un tableau de valeurs
@@ -58,14 +56,24 @@ class Decoder:
         Returns:
             dct_tab: tableau de valeurs issues de la transformation en cosinus
         """
-        res2 = np.int_([])
+        
+        # On divise en 3 la suite de valeurs quantifiées pour avoir les 3 couches
+        quantized_data = np.array_split(quantized_data, 3)
+        
+        n = int(quantized_data[0].size ** (1 / 2))
+        
+        # tableau de sortie
+        res2 = np.zeros((n, n, 3), dtype=int)
+        
+        k = 0 # numéro de la couche actuelle (0 <= k <= 2)
+        
+        # normalement len(quantized_data) = 3
         for data in quantized_data:
             up = False
             # Position du curseur
             i, j = 0, 0
-            # Tableau de sortie
-            n = int(data.size ** (1 / 2))
-            res = np.int_(np.zeros((n, n)))
+            # couche qui va être mise à jour
+            res = np.zeros((n, n), dtype=int)
             # Pour chacun des points qui constituent l'image
             for t in range(data.size):
                 # On ajoute le point courant
@@ -95,10 +103,13 @@ class Decoder:
                             # Sinon on parcoure la diagonale
                             j -= 1
                             i += 1
+            
             # on veut renvoyer un tableau comprenant les 3 tableaux des couches
-            res2 = np.concatenate(res2, res)
+            res2[:, :, k] = res
+            
+            k += 1 # mise à jour du numéro de la couche à mettre à jour
         return res2
-
+    
     def decode_dct(self, dct_data):
         """
         Decode un tableau de valeurs passées par la transformée discrète en
@@ -110,5 +121,6 @@ class Decoder:
         Returns:
             img_data: tableau de valeurs représentant l'image
         """
-
+        
         return NotImplementedError
+
