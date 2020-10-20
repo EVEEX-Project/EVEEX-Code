6ca5e6badf81c66463e2a27f1d6b657c487b9f31
JeanNoelClink <70959111+JeanNoelClink@users.noreply.github.com>
2020-10-20 10:15:45 +0200

    Fixs mineurs + rajout affichages

--- python_prototype/main.py
+++ python_prototype/main.py
@@ -24,29 +24,46 @@ log.set_log_level(LogLevel.DEBUG)
 # # # -------------------------IMAGE GENERATION-------------------------- # # #
 
 
-#img_gen = BlankImageGenerator(size=(10, 10))
 img_gen = MosaicImageGenerator(size=(10, 10), bloc_size=(2, 2))
+#img_gen = BlankImageGenerator(size=(10, 10))
+
 image = img_gen.generate()
 
 
 # # # -------------------------IMAGE ENCODING-------------------------- # # #
 
 
+img_visu = ImageVisualizer()
 enc = Encoder()
 
+# affichage 0
+print("\n\n\nEncodage - Image avant DCT (RGB) :\n")
+img_visu.show_image_with_matplotlib(image[:, :, 0])
+
 image_yuv = enc.RGB_to_YUV(image)
+
+# affichage 1
+print("\n\n\nEncodage - Image avant DCT (YUV) :\n")
+img_visu.show_image_with_matplotlib(image_yuv[:, :, 0])
+
 dct_data = enc.apply_DCT(image_yuv)
-zigzag_data_line = enc.zigzag_linearisation(dct_data[:, :, 0])
+
+# affichage 2
+print("\n\n\nEncodage - Image juste après DCT :\n")
+img_visu.show_image_with_matplotlib(dct_data[:, :, 0])
+print("\n\n\n")
+
+zigzag_data_line = enc.zigzag_linearisation(dct_data)
 quantized_data = enc.quantization(zigzag_data_line, threshold=DEFAULT_QUANTIZATION_THRESHOLD)
 rle_data = enc.run_level(quantized_data)
-compressed_data = enc.huffman_encode(rle_data)
+#compressed_data = enc.huffman_encode(rle_data)
 
 
 # # # -------------------------SENDING DATA OVER NETWORK-------------------------- # # #
 
 
 puiss_2_random = randint(10, 12)
-bufsize = 2 ** puiss_2_random
+bufsize = 2 ** puiss_2_random # doit impérativement être >= 51 (en pratique : OK)
 
 HOST = 'localhost'
 PORT = randint(5000, 15000)
@@ -65,7 +82,7 @@ serv.listen_for_packets(cli, callback=on_received_data)
 
 frame_id = randint(0, 65535)
 img_size = image.shape[0] * image.shape[1]
-macroblock_size = 5
+macroblock_size = 5 # par exemple
 
 bit_sender = BitstreamSender(frame_id, img_size, macroblock_size, rle_data, cli, bufsize)
 bit_sender.send_frame_RLE()
@@ -78,8 +95,15 @@ dec = Decoder()
 
 dec_rle_data = dec.decode_bitstream_RLE(received_data)
 dec_quantized_data = dec.decode_run_length(dec_rle_data)
-dec_zigzag_data = dec.decode_zigzag(dec_quantized_data)
-#dec_dct_data = dec.decode_dct(dec_zigzag_data)
+dec_dct_data = dec.decode_zigzag(dec_quantized_data)
+
+# affichage 3
+print("\n\n\nDécodage - Données DCT  de l'image :\n")
+img_visu.show_image_with_matplotlib(dec_dct_data[:, :, 0])
+print("\n")
+
+#dec_yuv_image = dec.decode_dct(dec_dct_data)
+# --> YUV to RGB (?)
 
 
 # # # -------------------------VISUALIZING THE IMAGE-------------------------- # # #
