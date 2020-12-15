dd873e9a7ca5af86d6f24fcdd4c2a4be9b612e95
nightlyside <nightlyside@gmail.com>
2020-12-15 11:52:25 +0100

    [C] Fix test unit + essai avec plus grande image

--- C-POO/c_prototype/test/image_test.c
+++ C-POO/c_prototype/test/image_test.c
@@ -1,24 +1,37 @@
 #include <stdlib.h>
+#include <time.h>
 
 #include "../types/Object.h"
 #include "../types/Image.h"
+
 
 int main() {
     printf("===============IMAGE TEST===============\n");
 
     struct Image *img, *gray, *sepia;
+    clock_t t;
+    double time_taken;
 
-    img = (struct Image *) loadImg("assets/image_res.jpg");
-    printf("Image loaded: ");
+    t = clock();
+    img = (struct Image *) loadImg("assets/image_res_high.jpg");
+    t = clock() - t;
+    time_taken = ((double)t) / CLOCKS_PER_SEC;
+    printf("Image loaded in %.2fms: ", time_taken * 1000);
     puto(img, stdout);
 
     gray = (struct Image *) toGray(img);
     sepia = (struct Image *) toSepia(img);
 
+    t = clock();
     saveImg(gray, "assets/gray_res.jpg");
-    printf("Saving gray img to disk\n");
-    saveImg(gray, "assets/sepia_res.jpg");
-    printf("Saving sepia img to disk\n");
+    t = clock() - t;
+    time_taken = ((double)t) / CLOCKS_PER_SEC;
+    printf("Saving gray img to disk in %.2fms\n", time_taken * 1000);
+    t = clock();
+    saveImg(sepia, "assets/sepia_res.jpg");
+    t = clock() - t;
+    time_taken = ((double)t) / CLOCKS_PER_SEC;
+    printf("Saving sepia img to disk in %.2fms\n", time_taken * 1000);
 
     delete(img);
     delete(gray);
