28e2f4ca2faea0d01af4beac87739b6340e6ea56
Alexandre F <nightlyside@gmail.com>
2021-01-12 15:54:34 +0100

    [C] Adding encoding dict generation

--- c_prototype/lib/huffman.c
+++ c_prototype/lib/huffman.c
@@ -1,14 +1,14 @@
 #include <malloc.h>
 #include <string.h>
+#include <assert.h>
 
 #include "huffman.h"
 
 #include "../types/Dictionary.h"
 #include "../types/DictionaryItem.h"
 #include "../types/Native.h"
+#include "../types/Node.h"
 
-#include "../types/Node.r"
-#include "../types/List.r"
 #include "../types/DictionaryItem.r"
 #include "../types/Native.r"
 
@@ -94,8 +94,142 @@ struct Node *getLowestFrequencySymbol(struct List *nodeList) {
 
 struct Node *generateTreeFromList(struct List *nodeList) {
     struct Node *n1, *n2, *n12;
+    struct Object *removed;
     while (count(nodeList) > 1) {
         n1 = getLowestFrequencySymbol(nodeList);
+        removed = removeItem(nodeList, (const struct Object *) n1);
+        assert(removed);
+        n2 = getLowestFrequencySymbol(nodeList);
+        removed = removeItem(nodeList, (const struct Object *) n2);
+        assert(removed);
 
+        // merging the two nodes
+        n12 = mergeTwoNodes(n1, n2);
+        addLast(nodeList, (const struct Object *) n12);
+    }
+
+    struct Node *racine = cast(Node(), lookAt(nodeList, 0));
+    return racine;
+}
+
+static int _print_t(struct Node *node, int is_left, int offset, int depth, char s[20][255])
+{
+    char b[20];
+    int width = 3;
+
+    // si the node is null there is nothing to show
+    if (!node) return 0;
+
+    // if the node has no parent
+    if (!node->left && !node->right) {
+        struct Native *val = cast(Native(), lookAt(node->value, 0));
+        // storing the nodes value in a buffer
+        sprintf(b, "(%s)", (char *) val->value);
+    }
+        // the node has a parent we show the branch
+    else
+        sprintf(b, " | ");
+
+    // calculating left and right offset to display the branches
+    int left  = _print_t(node->left, 1, offset, depth + 1, s);
+    int right = _print_t(node->right, 0, offset + left + width, depth + 1, s);
+
+    //printf("Node : %s, Depth : %d, Left : %d, right : %d, offset : %d\n", node->value, depth, left, right, offset);
+    // showing the nodes value
+    for (int i = 0; i < width; i++) {
+        // if we haven't reached the end of the buffer
+        if (b[i] != '\0')
+            s[2 * depth][offset + left + i] = b[i];
+        else
+            break;
+    }
+
+    // if this is not the first layer and the child is left one from parent
+    if (depth && is_left) {
+        // printing the horizontal branch
+        for (int i = 0; i < width + right; i++)
+            s[2 * depth - 1][offset + left + width/2 + i] = '-';
+
+        // printing the end of the branch
+        s[2 * depth - 1][offset + left + width/2] = '+';
+        s[2 * depth - 1][offset + left + width + right + width/2] = '+';
+
+    }
+        // if this is not the first layer and the child is right one from parent
+    else if (depth && !is_left) {
+        // printing the horizontal branch
+        for (int i = 0; i < left + width; i++)
+            s[2 * depth - 1][offset - width/2 + i] = '-';
+
+        // printing the end of the branch
+        s[2 * depth - 1][offset + left + width/2] = '+';
+        s[2 * depth - 1][offset - width/2 - 1] = '+';
+    }
+
+    // returning the space used by this side of the branch
+    return left + width + right;
+}
+
+void printHuffmanTree(struct Node *root) {
+    // preparing the string that will contain the representation
+    char s[20][255];
+    for (int i = 0; i < 20; i++)
+        for (int j = 0; j < 255; j++)
+            s[i][j] = ' ';
+
+    // getting the tree in the string
+    _print_t(root, 0, 0, 0, s);
+
+    // printing the string to the screen
+    for (int i = 0; i < 20; i++) {
+        int empty = 1;
+        int c = 0;
+        // checking if a row is empty
+        while (s[i][c] != '\0' && c < 255) {
+            if (s[i][c] != ' ') {
+                empty = 0;
+                break;
+            }
+            c++;
+        }
+
+        // if the row is not empty then print it on the screen
+        if (!empty) {
+            for (int j = 0; j < 255; j++) {
+                printf("%c", s[i][j]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+void generateEncodingDict(struct Dictionary *encoding, struct Node *root, char *prefix) {
+    // if there is no children we add the symbol to the dictionary
+    if (root->left == NULL && root->right == NULL) {
+        // getting the current prefix
+        char *newPrefix = strdup(prefix);
+        // updating its entry in the dictionary
+        set(encoding, newPrefix, root->value);
+        return;
+    }
+
+    // updating the size for the prefix
+    char newPrefix[strlen(prefix) + 2];
+    strcpy(newPrefix, prefix);
+    newPrefix[strlen(prefix) + 1] = '\0';
+
+    // if there is a child on the left
+    if (root->left != NULL) {
+        // updating the prefix with a "0"
+        newPrefix[strlen(prefix)] = '0';
+        // continue with the creation of the encoding dictionary
+        generateEncodingDict(encoding, root->left, newPrefix);
+    }
+    // if there is a child on the right
+    if (root->right != NULL) {
+        // updating the prefix with a "1"
+        newPrefix[strlen(prefix)] = '1';
+        // continue with the creation of the encoding dictionary
+        generateEncodingDict(encoding, root->right, newPrefix);
     }
 }
--- c_prototype/lib/huffman.h
+++ c_prototype/lib/huffman.h
@@ -1,12 +1,17 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 
+#include "../types/Node.r"
+#include "../types/Dictionary.r"
+
 #include "../types/Object.h"
-#include "../types/Node.h"
 #include "../types/List.h"
 
 struct Node *mergeTwoNodes(struct Node *a, struct Node *b);
 struct List *splitPhraseInNodes(const char *phrase, void *_symbols);
 struct Node *getLowestFrequencySymbol(struct List *nodeList);
+struct Node *generateTreeFromList(struct List *nodeList);
+void printHuffmanTree(struct Node *root);
+void generateEncodingDict(struct Dictionary *encoding, struct Node *root, char *prefix);
 
 #endif
