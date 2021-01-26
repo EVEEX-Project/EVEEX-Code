71969f0e516d827a9b9379497dd94b6e42bc789d
Hussein <husseinsaadgs@gmail.com>
2021-01-26 12:05:30 +0100

    ajouter dct idct dans le fichier encoder.c et encoder.h

--- c_prototype/lib/encoder.c
+++ c_prototype/lib/encoder.c
@@ -4,11 +4,22 @@
 
 #include <stdlib.h>
 #include <assert.h>
-
+#include <stdio.h>
+#include <math.h>
 #include "encoder.h"
 #include "huffman.h"
 #include "../types/Image.h"
 #include "../types/Image.r"
+
+
+
+
+#define N 16
+#define PI 3.141592653589793
+
+
+
+
 
 const struct Image *toYUVImage(const void *_self) {
     const struct Image *self = cast(Image(), _self);
@@ -90,3 +101,68 @@ const struct List *splitInMacroblocs(const void *_self, int size) {
 
     return macroblocs;
 }
+
+
+
+
+
+
+
+void DCT_N(int pixel[N][N], float Coeff_DCT[N][N]);
+void IDCT_N(float C[N][N], int reconstructed_matrix[N][N]);
+
+/*int main(int argc, char const *argv[])
+{
+    int i=0,j=0,k=0;
+
+    float coeff_DCT[N][N]={0};//matrix to put the coefficients of DCT
+    int reconstructed_matrix[N][N]={0}; //Matrix to put the reconstructed matix
+    //Matrix representing the signal
+    int matrix[N][N]={{244,243,118,154,126,246,137,184,244,243,118,154,126,246,137,184},
+                      {178,127,196,155,184,184,147,245,244,243,118,154,126,246,137,184},
+                      {114,116,134,231,145,166,214,112,244,243,118,154,126,246,137,184},
+                      {178,127,196,155,184,184,247,245,244,243,118,154,126,246,137,184},
+                      {200,224,133,145,105,167,237,118,244,243,118,154,126,246,137,184},
+                      {137,263,118,154,126,246,137,89,244,243,118,154,126,246,137,184},
+                      {178,127,196,255,184,184,147,245,244,243,118,154,126,246,137,184},
+                      {114,116,134,231,145,166,247,198,244,243,118,154,126,246,137,184},
+                      {244,243,118,154,126,246,137,184,244,243,118,154,126,246,137,184},
+                      {178,127,196,155,184,184,147,245,244,243,118,154,126,246,137,184},
+                      {114,116,134,231,145,166,214,112,244,243,118,154,126,246,137,184},
+                      {178,127,196,155,184,184,247,245,244,243,118,154,126,246,137,184},
+                      {200,224,133,145,105,167,237,118,244,243,118,154,126,246,137,184},
+                      {137,263,118,154,126,246,137,89,244,243,118,154,126,246,137,184},
+                      {178,127,196,255,184,184,147,245,244,243,118,154,126,246,137,184},
+                      {114,116,134,231,145,166,247,198,244,243,118,154,126,246,137,184}};
+
+    //Calculate the origin matrix of DCT
+    //Put the coefficients in coeff_DCT
+    DCT_N(matrix, coeff_DCT);
+
+    //Calculate the IDCT using the Coefficients matrix
+    //Put the regenerated signal in the reconstructed matrix
+    IDCT_N(coeff_DCT, reconstructed_matrix);
+
+
+    //printing the different steps
+    for(k=0;k<4;k++)
+    {
+        if(k==0)puts("\nOrigin Matrix :");
+        if(k==1)puts("\nCoefficients Matrix :");
+        if(k==2)puts("\nReconstructed Matrix :");
+        if(k==3)puts("\nDifference Origin/Reconstructed:");
+
+        for(i=0;i<N;i++)
+        {
+            for(j=0;j<N;j++)
+            {
+                if(k==0)printf("%d ",matrix[i][j]);
+                if(k==1)printf("%5.1f ",coeff_DCT[i][j]);
+                if(k==2)printf("%d ",reconstructed_matrix[i][j]);
+                if(k==3)printf("%d ",matrix[i][j]-reconstructed_matrix[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}*/
--- c_prototype/lib/encoder.h
+++ c_prototype/lib/encoder.h
@@ -5,6 +5,89 @@
 #ifndef C_PROTOTYPE_ENCODER_H
 #define C_PROTOTYPE_ENCODER_H
 
+#define PI 3.141592653589793
+#define N 16
+
+void DCT_N(int pixel[N][N], float Coeff_DCT[N][N])
+{
+    int u=0, v=0;
+    int x=0, y=0;
+    float au=0, av=0;
+
+    for(v=0;v<=N-1;v++)
+    {
+        for(u=0;u<=N-1;u++)
+        {
+            //Calculate the double sum
+            for(y=0;y<=N-1;y++)
+            {
+                for(x=0;x<=N-1;x++)
+                {
+                    Coeff_DCT[v][u]+=pixel[y][x]*(cos((PI*(x+0.5)*u)/N)*cos((PI*(y+0.5)*v)/N));
+                }
+            }
+
+            //Calculate the orthogonality factors
+            if(u==0){
+                au=(1/sqrt(2));
+            }
+            else {
+                au=1;
+            }
+
+            if(v==0){
+                av=(1/sqrt(2));
+            }
+            else{
+                av=1;
+            }
+
+            //Calculate the complete coefficients
+            Coeff_DCT[v][u]=Coeff_DCT[v][u]*0.25*au*av;
+        }
+    }
+
+}
+
+void IDCT_N(float Coeff_DCT[N][N], int reconstructed_matrix[N][N])
+{
+    int u=0, v=0;
+    int x=0, y=0;
+    float au=0, av=0;
+    //Intermediate matrix to work with real numbers
+    float f[N][N]={0};
+
+    for(y=0;y<=N-1;y++)
+    {
+        for(x=0;x<=N-1;x++)
+        {
+            for(v=0;v<=N-1;v++)
+            {
+                for(u=0;u<=N-1;u++)
+                {
+                    if(u==0){
+                        au=1/sqrt(2);
+                    }
+                    else {
+                        au=1;
+                    }
+
+                    if(v==0){
+                        av=1/sqrt(2);
+                    }
+                    else{
+                        av=1;
+                    }
+                    f[y][x]+=Coeff_DCT[v][u]*au*av*cos(((x+0.5)*PI*u)/N)*cos(((y+0.5)*PI*v)/N);
+                }
+            }
+            f[y][x]*=0.25;
+            //conversion of real integers
+            reconstructed_matrix[y][x]=floor(f[y][x]+0.5);
+        }
+    }
+}
+
 const struct Image *toYUVImage(const void *self);
 const struct List *splitInMacroblocs(const void *self, int size);
 
