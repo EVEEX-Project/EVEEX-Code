15a5f81a84a8c98bddfc7cc79270690c4dd206a1
nightlyside <nightlyside@gmail.com>
2021-02-02 14:43:33 +0100

    [C] Hello world

--- c_prototype/main.c
+++ c_prototype/main.c
@@ -6,6 +6,20 @@
 #include "lib/huffman.h"
 #include "lib/encoder.h"
 
+void helloWorld(char* version) {
+    printf("\033[1;35m            _______    _______________  __\n");
+    printf("           / ____/ |  / / ____/ ____/ |/ /\n");
+    printf("          / __/  | | / / __/ / __/  |   /\n");
+    printf("         / /___  | |/ / /___/ /___ /   |\n");
+    printf("        /_____/  |___/_____/_____//_/|_|\n");
+    printf("          ___  ___  / __/ _ \\/ ___/ _ |\n");
+    printf("         / _ \\/ _ \\/ _// ___/ (_ / __ |\n");
+    printf("         \\___/_//_/_/ /_/   \\___/_/ |_|\n\n");
+    printf("\033[0;36m32-bit RISC-V Open Source video compression program\n");
+    printf("                version \033[0;31m%s\033[0m\n\n", version);
+    printf("─────────────────────\033[1;37m LOGS \033[0m────────────────────────\n");
+}
+
 void saveMacroBlocksToDisk(const struct List *macroBlocks) {
     for (int k = 0; k < count(macroBlocks); k++) {
         char filename[64];
@@ -15,6 +29,9 @@ void saveMacroBlocksToDisk(const struct List *macroBlocks) {
 }
 
 int main() {
+    // Printing pretty hello world
+    helloWorld("1.2.0");
+
     // Loading the image
     struct Image *img;
     img = (struct Image *) loadImg("assets/image_res.png");
